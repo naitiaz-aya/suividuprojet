690deaf421661f393dc90b7e6e6cd124df54593c
naitiaz-aya <bntsaidaya@gmail.com>
2021-12-06 19:49:30 +0100

    modification

--- SUIVI_DE_PROJET.sql
+++ SUIVI_DE_PROJET.sql
@@ -1,56 +1,34 @@
-CREATE TABLE "Project" (
-  "Code" int,
-  "label" string,
-  "StartDate" date,
-  "EndDate" date,
-  "EmloyeesId" int,
-  "PhaseId" int,
-  "BillingStatus" bolean,
-  "AchievStatus" bolean,
-  "PayeStatus" bolean,
-  "Price" int,
-  PRIMARY KEY ("Code")
-);
-
-CREATE TABLE "Employees" (
-  "id" int,
-  "FName" string,
-  "LName" string,
-  "TelNumb" int,
-  "Email" string,
-  "Password" string,
-  "login" string,
-  "role" string,
-  PRIMARY KEY ("id"),
-  CONSTRAINT "FK_Employees.TelNumb"
-    FOREIGN KEY ("TelNumb")
-      REFERENCES "Project"("EmloyeesId")
-);
-
-CREATE TABLE "Organisme" (
+CREATE TABLE "Delivrable" (
   "code" int,
-  "Name" string,
-  "Adress" string,
-  "TelNumb" int,
-  "Email" string,
-  "website" string,
-  "ContactNumb" int,
+  "label" VARCHAR(255),
+  "Description" VARCHAR(255),
+  "WayToDoc" VARCHAR(255),
   PRIMARY KEY ("code")
 );
 
 CREATE TABLE "Phase" (
   "id" int,
-  "Document" string,
+  "Document" VARCHAR(255),
   "DelivrableId" int,
+  PRIMARY KEY ("id"),
+  CONSTRAINT "FK_Phase.DelivrableId"
+    FOREIGN KEY ("DelivrableId")
+      REFERENCES "Delivrable"("code")
+);
+
+CREATE TABLE "Directeur" (
+  "id" int,
   PRIMARY KEY ("id")
 );
 
-CREATE TABLE "Delivrable" (
-  "code" int,
-  "label" string,
-  "Description" string,
-  "WayToDoc" string,
-  PRIMARY KEY ("code")
+CREATE TABLE "Comptable" (
+  "id" int,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Chef" (
+  "id" int,
+  PRIMARY KEY ("id")
 );
 
 CREATE TABLE "Sectretaire" (
@@ -58,31 +36,55 @@ CREATE TABLE "Sectretaire" (
   PRIMARY KEY ("id")
 );
 
-CREATE TABLE "Chef" (
-  "id" int,
-  PRIMARY KEY ("id"),
-  CONSTRAINT "FK_Chef.id"
-    FOREIGN KEY ("id")
-      REFERENCES "Employees"("Email")
-);
-
-CREATE TABLE "Directeur" (
-  "id" int,
-  PRIMARY KEY ("id"),
-  CONSTRAINT "FK_Directeur.id"
-    FOREIGN KEY ("id")
-      REFERENCES "Employees"("Email")
-);
-
-CREATE TABLE "Comptable" (
-  "id" int,
-  PRIMARY KEY ("id"),
-  CONSTRAINT "FK_Comptable.id"
-    FOREIGN KEY ("id")
-      REFERENCES "Employees"("Email")
-);
-
 CREATE TABLE "Admin" (
   "id" int,
   PRIMARY KEY ("id")
 );
+
+CREATE TABLE "Employees" (
+  "id" int,
+  "FName" VARCHAR(255),
+  "LName" VARCHAR(255),
+  "TelNumb" int,
+  "Email" VARCHAR(255),
+  "Password" VARCHAR(255),
+  "login" VARCHAR(255),
+  "role" VARCHAR(255),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Organisme" (
+  "code" int,
+  "Name" VARCHAR(255),
+  "Adress" VARCHAR(255),
+  "TelNumb" int,
+  "Email" VARCHAR(255),
+  "website" VARCHAR(255),
+  "ContactNumb" int,
+  PRIMARY KEY ("code")
+);
+
+CREATE TABLE "Project" (
+  "Code" int,
+  "label" VARCHAR(255),
+  "organismeCode" int,
+  "StartDate" date,
+  "EndDate" date,
+  "EmloyeesId" int,
+  "PhaseId" int,
+  "BillingStatus" Boolean,
+  "AchievStatus" Boolean,
+  "PayeStatus" Boolean,
+  "Price" int,
+  PRIMARY KEY ("Code"),
+  CONSTRAINT "FK_Project.PhaseId"
+    FOREIGN KEY ("PhaseId")
+      REFERENCES "Phase"("id"),
+  CONSTRAINT "FK_Project.EmloyeesId"
+    FOREIGN KEY ("EmloyeesId")
+      REFERENCES "Employees"("id"),
+  CONSTRAINT "FK_Project.organismeCode"
+    FOREIGN KEY ("organismeCode")
+      REFERENCES "Organisme"("code")
+);
+
